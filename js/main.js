b72bc345636386d7a33785a56516cb6ba341d66a
Troy Dorman <troydorman@gmail.com>
2021-06-05 07:18:06 -0600

    finished implementing our on-hover animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
 
 left.addEventListener("mouseenter", () => {
   container.classList.add("hover-left");
